589ebe6837af049f5544582fb2a129a39f74a594
Noah Korner <noahskorner@gmail.com>
2022-04-13 21:49:07 -0500

    Created server dropdown.

---
+++ src/components/layouts/app-layout/sidebar/server-header.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import useServer from '../../../../utils/hooks/use-server';
+import ChevronDownIcon from '../../../icons/chevron-down.svg';
+import CloseIcon from '../../../icons/close.svg';
+import UserAddIcon from '../../../icons/user-add.svg';
+import SettingsIcon from '../../../icons/settings.svg';
+import { CSSTransition } from 'react-transition-group';
+
+const SidebarHeader = () => {
+  const { server } = useServer();
+  const [showServerMenu, setShowServerMenu] = useState(false);
+
+  const handleServerMenuBtnClick = () => {
+    setShowServerMenu((prev) => !prev);
+  };
+
+  return (
+    <div className="relative h-full w-full">
+      <button
+        onClick={handleServerMenuBtnClick}
+        className="flex h-full w-full items-center justify-between py-2 px-4 hover:bg-slate-600"
+      >
+        <h5 className="max-w-80 truncate text-sm font-bold">{server?.name}</h5>
+        <div className="flex h-4 w-4 items-center justify-center">
+          {showServerMenu ? <CloseIcon /> : <ChevronDownIcon />}
+        </div>
+      </button>
+      <CSSTransition
+        in={showServerMenu}
+        timeout={200}
+        classNames="fade-in"
+        unmountOnExit
+      >
+        <div className="absolute top-full mt-2 w-full px-3">
+          <div className="h-full w-full rounded-md bg-slate-1100 p-2 shadow-xl">
+            <button className="flex w-full items-center justify-between rounded p-2 text-xs font-medium text-indigo-400 hover:bg-indigo-600 hover:text-white">
+              <span>Invite People</span>
+              <div className="justfiy-center flex h-4 w-4 items-center">
+                <UserAddIcon />
+              </div>
+            </button>
+            <button className="flex w-full items-center justify-between rounded p-2 text-xs font-medium hover:bg-indigo-600 hover:text-white">
+              <span>Server Settings</span>
+              <div className="justfiy-center flex h-4 w-4 items-center">
+                <SettingsIcon />
+              </div>
+            </button>
+            <button className="flex w-full items-center justify-between rounded p-2 text-xs font-medium hover:bg-indigo-600 hover:text-white">
+              <span>Create Channel</span>
+              <div className="justfiy-center flex h-4 w-4 items-center">
+                <SettingsIcon />
+              </div>
+            </button>
+            <button className="flex w-full items-center justify-between rounded p-2 text-xs font-medium hover:bg-indigo-600 hover:text-white">
+              <span>Edit Server Profile</span>
+              <div className="justfiy-center flex h-4 w-4 items-center">
+                <SettingsIcon />
+              </div>
+            </button>
+          </div>
+        </div>
+      </CSSTransition>
+    </div>
+  );
+};
+
+export default SidebarHeader;
--- src/components/layouts/app-layout/sidebar/sidebar.tsx
+++ src/components/layouts/app-layout/sidebar/sidebar.tsx
@@ -1,10 +1,13 @@
 import useWindowSize from '../../../../utils/hooks/use-window-size';
 import SidebarBody from './sidebar-body';
 import SidebarFooter from './sidebar-footer';
-import SidebarHeader from './sidebar-header';
+import ServerHeader from './server-header';
+import { useRouter } from 'next/router';
 
 const Sidebar = () => {
   const { heightStyle } = useWindowSize();
+  const router = useRouter();
+  const isServerPage = router.pathname.toLowerCase().includes('server');
 
   return (
     <div
@@ -12,7 +15,7 @@ const Sidebar = () => {
       className="relative w-full overflow-hidden rounded-r-3xl bg-slate-800 md:w-sidebar md:rounded-r-none"
     >
       <div className="h-header shadow-header">
-        <SidebarHeader />
+        {isServerPage && <ServerHeader />}
       </div>
       <div className="h-body space-y-2 overflow-y-auto">
         <SidebarBody />
