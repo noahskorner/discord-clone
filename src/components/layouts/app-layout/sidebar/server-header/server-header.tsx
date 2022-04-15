5b7ecb1246f4c38c2623fa11ac659acc407e9956
Noah Korner <noahskorner@gmail.com>
2022-04-15 16:31:23 -0500

    Moved server-header component.

--- src/components/layouts/app-layout/sidebar/server-header/server-header.tsx
+++ src/components/layouts/app-layout/sidebar/server-header/server-header.tsx
@@ -1,9 +1,9 @@
 import { useRef, useState } from 'react';
-import useServer from '../../../../utils/hooks/use-server';
-import ChevronDownIcon from '../../../icons/chevron-down.svg';
-import CloseIcon from '../../../icons/close.svg';
-import UserAddIcon from '../../../icons/user-add.svg';
-import SettingsIcon from '../../../icons/settings.svg';
+import useServer from '../../../../../utils/hooks/use-server';
+import ChevronDownIcon from '../../../../icons/chevron-down.svg';
+import CloseIcon from '../../../../icons/close.svg';
+import UserAddIcon from '../../../../icons/user-add.svg';
+import SettingsIcon from '../../../../icons/settings.svg';
 import { CSSTransition } from 'react-transition-group';
 
 const SidebarHeader = () => {
@@ -41,25 +41,25 @@ const SidebarHeader = () => {
           className="absolute top-full z-50 mt-2 w-full px-3 focus:bg-red-50"
         >
           <div className="h-full w-full rounded-md bg-slate-1100 p-2 shadow-xl">
-            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium text-indigo-400 hover:bg-indigo-600 hover:text-white">
+            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium text-indigo-400 hover:bg-indigo-600 active:bg-indigo-800 hover:text-white">
               <span>Invite People</span>
               <div className="justfiy-center flex h-4 w-4 items-center">
                 <UserAddIcon />
               </div>
             </button>
-            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 hover:text-white">
+            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 active:bg-indigo-800 hover:text-white">
               <span>Server Settings</span>
               <div className="justfiy-center flex h-4 w-4 items-center">
                 <SettingsIcon />
               </div>
             </button>
-            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 hover:text-white">
+            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 active:bg-indigo-800 hover:text-white">
               <span>Create Channel</span>
               <div className="justfiy-center flex h-4 w-4 items-center">
                 <SettingsIcon />
               </div>
             </button>
-            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 hover:text-white">
+            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 active:bg-indigo-800 hover:text-white">
               <span>Edit Server Profile</span>
               <div className="justfiy-center flex h-4 w-4 items-center">
                 <SettingsIcon />
