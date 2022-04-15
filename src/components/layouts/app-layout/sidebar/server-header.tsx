c46cf7c52fb3265fddd65942294944f21022cd55
Noah Korner <noahskorner@gmail.com>
2022-04-15 16:28:01 -0500

    I hate modals

--- src/components/layouts/app-layout/sidebar/server-header.tsx
+++ src/components/layouts/app-layout/sidebar/server-header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import useServer from '../../../../utils/hooks/use-server';
 import ChevronDownIcon from '../../../icons/chevron-down.svg';
 import CloseIcon from '../../../icons/close.svg';
@@ -9,16 +9,20 @@ import { CSSTransition } from 'react-transition-group';
 const SidebarHeader = () => {
   const { server } = useServer();
   const [showServerMenu, setShowServerMenu] = useState(false);
+  const serverMenuRef = useRef(null);
 
   const handleServerMenuBtnClick = () => {
     setShowServerMenu((prev) => !prev);
+  };
+  const handleClickAway = () => {
+    setShowServerMenu(false);
   };
 
   return (
     <div className="relative h-full w-full">
       <button
         onClick={handleServerMenuBtnClick}
-        className="flex h-full w-full items-center justify-between py-2 px-4 hover:bg-slate-600"
+        className="relative z-50 flex h-full w-full items-center justify-between bg-slate-700 py-2 px-4 hover:bg-slate-600"
       >
         <h5 className="max-w-80 truncate text-sm font-bold">{server?.name}</h5>
         <div className="flex h-4 w-4 items-center justify-center">
@@ -29,9 +33,13 @@ const SidebarHeader = () => {
         in={showServerMenu}
         timeout={200}
         classNames="fade-in"
-        unmountOnExit
+        unmountOnExit={true}
+        ref={serverMenuRef}
       >
-        <div className="absolute top-full mt-2 w-full px-3">
+        <div
+          onBlur={() => console.log('wtf')}
+          className="absolute top-full z-50 mt-2 w-full px-3 focus:bg-red-50"
+        >
           <div className="h-full w-full rounded-md bg-slate-1100 p-2 shadow-xl">
             <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium text-indigo-400 hover:bg-indigo-600 hover:text-white">
               <span>Invite People</span>
@@ -60,6 +68,12 @@ const SidebarHeader = () => {
           </div>
         </div>
       </CSSTransition>
+      {showServerMenu && (
+        <div
+          onClick={handleClickAway}
+          className="fixed top-0 left-0 bottom-0 right-0 z-40 bg-transparent"
+        ></div>
+      )}
     </div>
   );
 };
