01e6246f48060556d6561a8d6aed6cf15e9e9a0b
Noah Korner <noahskorner@gmail.com>
2022-04-21 21:39:42 -0500

    Ran prettier on files.

--- src/components/layouts/app-layout/sidebar/server-header/server-header.tsx
+++ src/components/layouts/app-layout/sidebar/server-header/server-header.tsx
@@ -41,25 +41,25 @@ const SidebarHeader = () => {
           className="absolute top-full z-50 mt-2 w-full px-3 focus:bg-red-50"
         >
           <div className="h-full w-full rounded-md bg-slate-1100 p-2 shadow-xl">
-            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium text-indigo-400 hover:bg-indigo-600 active:bg-indigo-800 hover:text-white">
+            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium text-indigo-400 hover:bg-indigo-600 hover:text-white active:bg-indigo-800">
               <span>Invite People</span>
               <div className="justfiy-center flex h-4 w-4 items-center">
                 <UserAddIcon />
               </div>
             </button>
-            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 active:bg-indigo-800 hover:text-white">
+            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 hover:text-white active:bg-indigo-800">
               <span>Server Settings</span>
               <div className="justfiy-center flex h-4 w-4 items-center">
                 <SettingsIcon />
               </div>
             </button>
-            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 active:bg-indigo-800 hover:text-white">
+            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 hover:text-white active:bg-indigo-800">
               <span>Create Channel</span>
               <div className="justfiy-center flex h-4 w-4 items-center">
                 <SettingsIcon />
               </div>
             </button>
-            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 active:bg-indigo-800 hover:text-white">
+            <button className="flex w-full items-center justify-between rounded p-2 text-sm font-medium hover:bg-indigo-600 hover:text-white active:bg-indigo-800">
               <span>Edit Server Profile</span>
               <div className="justfiy-center flex h-4 w-4 items-center">
                 <SettingsIcon />
