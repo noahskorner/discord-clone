cb219b7d7420bfa3c065c277c56fdaf60e061ef6
Noah Korner <noahskorner@gmail.com>
2022-04-28 20:03:14 -0500

    Peers connecting.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,33 @@
 import type { NextPage } from 'next';
+import { useEffect, useRef } from 'react';
 import AppLayout from '../components/layouts/app-layout';
 import useRTC from '../utils/hooks/use-rtc';
 
+const IndexContent = () => {
+  const localVideoRef = useRef<HTMLVideoElement>(null);
+  const remoteVideoRef = useRef<HTMLVideoElement>(null);
+  const { localStream, remoteStream, startCall } = useRTC();
+
+  useEffect(() => {
+    if (localVideoRef.current == null) return;
+    if (remoteVideoRef.current == null) return;
+    localVideoRef.current.srcObject = localStream;
+    remoteVideoRef.current.srcObject = remoteStream;
+  }, [localStream, remoteStream]);
+
+  return (
+    <div>
+      <button onClick={startCall}>Start call</button>
+      <video ref={localVideoRef} muted autoPlay playsInline></video>
+      <video ref={remoteVideoRef} autoPlay playsInline></video>
+    </div>
+  );
+};
+
 const IndexPage: NextPage = () => {
-  const { startCall } = useRTC();
   return (
     <AppLayout>
-      <div>
-        <button onClick={() => startCall()}>Start call</button>
-      </div>
+      <IndexContent />
     </AppLayout>
   );
 };
--- src/utils/contexts/rtc-context.tsx
+++ src/utils/contexts/rtc-context.tsx
@@ -5,10 +5,14 @@ import useSocket from '../hooks/use-socket';
 
 interface RTCContextInterface {
   startCall: () => Promise<void>;
+  localStream: null | MediaStream;
+  remoteStream: null | MediaStream;
 }
 
 const defaultValues = {
   startCall: async () => {},
+  localStream: null,
+  remoteStream: null,
 };
 
 export const RTCContext = createContext<RTCContextInterface>(defaultValues);
@@ -24,11 +28,21 @@ const RTC_OPTIONS = {
 
 export const RTCProvider = ({ children }: RTCProviderInterface) => {
   const peerConnectionRef = useRef<null | RTCPeerConnection>(null);
+  const localStreamRef = useRef<null | MediaStream>(null);
+  const remoteStreamRef = useRef<null | MediaStream>(null);
 
   const { socket } = useSocket();
 
-  const initPeerConnection = useCallback(() => {
+  const initPeerConnection = useCallback(async () => {
     const peerConnection = new RTCPeerConnection(stunConfig);
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    localStreamRef.current = stream;
+    stream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, stream);
+    });
     peerConnection.addEventListener('icecandidate', (event) => {
       if (event.candidate) {
         socket?.current.emit(EventEnum.SEND_ICE_CANDIDATE, event.candidate);
@@ -39,26 +53,38 @@ export const RTCProvider = ({ children }: RTCProviderInterface) => {
         console.log('peers connected!');
       }
     });
+    peerConnection.addEventListener('track', (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+        remoteStreamRef.current?.addTrack(track);
+      });
+    });
 
     return peerConnection;
   }, [socket]);
 
-  useEffect(() => {
-    const receiveOfferHandler = async (offer: RTCSessionDescriptionInit) => {
-      peerConnectionRef.current = initPeerConnection();
-      peerConnectionRef.current.setRemoteDescription(offer);
+  const receiveOfferHandler = useCallback(
+    async (offer: RTCSessionDescriptionInit) => {
+      peerConnectionRef.current = await initPeerConnection();
+      await peerConnectionRef.current.setRemoteDescription(offer);
 
       const answer = await peerConnectionRef.current.createAnswer(RTC_OPTIONS);
       await peerConnectionRef.current.setLocalDescription(answer);
-    };
-    const receiveAnswerHandler = async (answer: RTCSessionDescription) => {
-      const remoteDesc = new RTCSessionDescription(answer);
-      await peerConnectionRef.current?.setRemoteDescription(remoteDesc);
-    };
-    const receiveCandidateHandler = async (candidate: RTCIceCandidate) => {
-      await peerConnectionRef.current?.addIceCandidate(candidate);
-    };
 
+      socket?.current.emit(EventEnum.SEND_ANSWER, answer);
+    },
+    [initPeerConnection, socket],
+  );
+
+  const receiveAnswerHandler = async (answer: RTCSessionDescription) => {
+    const remoteDesc = new RTCSessionDescription(answer);
+    await peerConnectionRef.current?.setRemoteDescription(remoteDesc);
+  };
+
+  const receiveCandidateHandler = async (candidate: RTCIceCandidate) => {
+    await peerConnectionRef.current?.addIceCandidate(candidate);
+  };
+
+  useEffect(() => {
     socket?.current.on(EventEnum.RECEIVE_OFFER, receiveOfferHandler);
     socket?.current.on(EventEnum.RECEIVE_ANSWER, receiveAnswerHandler);
     socket?.current.on(
@@ -74,17 +100,26 @@ export const RTCProvider = ({ children }: RTCProviderInterface) => {
         receiveCandidateHandler,
       );
     };
-  }, [initPeerConnection, socket]);
+  }, [initPeerConnection, receiveOfferHandler, socket]);
 
   const startCall = async () => {
-    peerConnectionRef.current = initPeerConnection();
+    peerConnectionRef.current = await initPeerConnection();
     const offer = await peerConnectionRef.current.createOffer(RTC_OPTIONS);
     await peerConnectionRef.current.setLocalDescription(offer);
 
     // kick everything off
     socket?.current.emit(EventEnum.SEND_OFFER, offer);
   };
+
   return (
-    <RTCContext.Provider value={{ startCall }}>{children}</RTCContext.Provider>
+    <RTCContext.Provider
+      value={{
+        localStream: localStreamRef.current,
+        remoteStream: remoteStreamRef.current,
+        startCall,
+      }}
+    >
+      {children}
+    </RTCContext.Provider>
   );
 };
